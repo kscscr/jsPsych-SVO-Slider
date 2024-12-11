44b4aa7d5fd9854fda55a2713dc594e652b5a940
kscscr <63399643+kscscr@users.noreply.github.com>
2024-12-11 22:42:08 +0900

    Add files via upload

---
+++ README_SVO_ja.md
@@ -0,0 +1,85 @@
+# jsPsychでSVO Slider (Murphy et al., 2011) を実施するプラグイン
+## 使い方
+### ファイルの配置
+以下の2つのファイルを、distフォルダに配置する。
+- Plugins: plugin-html-slider-svo-multi.js
+- CSS: svo.css
+
+### プログラムの実行方法
+headタグに以下の記述を追加する。
+```
+<script src="dist/plugin-html-slider-svo-multi.js"></script>
+<link rel="stylesheet" href="svo.css">
+```
+
+実験の実施方法は次の通り。
+- questions:stimulus_upperとstimulus_lowerに、それぞれ9個の配列を設置する。
+- require_movement: スライダーを一度動かさないと、次の画面に進めないようにする
+- prompt: 質問の文章。
+- slider_width: 500px以下で指定可能。
+- label_upper/label_lower：上のラベルと下のラベル
+
+```
+var trial = {
+      type: jsPsychHtmlSliderSVOMulti,
+      questions: [
+        {
+          stimulus_upper: [85, 85, 85, 85, 85, 85, 85, 85, 85], 
+          stimulus_lower: [85, 76, 68, 59, 50, 41, 33, 24, 15]
+        }, 
+        {
+          stimulus_upper: [85, 87, 89, 91, 93, 94, 96, 98, 100], 
+          stimulus_lower: [15, 19, 24, 28, 33, 37, 41, 46, 50]
+        },
+        {
+          stimulus_upper: [50, 54, 59, 63, 68, 72, 76, 81, 85], 
+          stimulus_lower: [100, 98, 96, 94, 93, 91, 89, 87, 85]
+        }, 
+
+        {
+          stimulus_upper: [50, 54, 59, 63, 68, 72, 76, 81, 85], 
+          stimulus_lower: [100, 89, 79, 68, 58, 47, 36, 26, 15]
+        },
+        {
+          stimulus_upper: [100, 94, 88, 81, 75, 69, 63, 56, 50], 
+          stimulus_lower: [50, 56, 63, 69, 75, 81, 88, 94, 100]
+        }, 
+        {
+          stimulus_upper: [100, 98, 96, 94, 93, 91, 89, 87, 85], 
+          stimulus_lower: [50, 54, 59, 63, 68, 72, 76, 81, 85]
+        },
+      ],
+      require_movement: true,
+      label_upper: "You</br>receive", 
+      label_lower: "Other</br>receive", 
+      slider_width: 500,
+      prompt: '<p>You will allocate resources between yourself and others. </br>Please select the option that best matches your preference.</p>'
+};
+```
+
+回答データは、survey-multi-choiceプラグインなどと同様に、配列の形で保存される。
+
+
+
+## ライセンス
+The MIT License (MIT)
+
+Copyright (c) 2014-2022 Joshua R. de Leeuw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ plugin-html-slider-svo-multi.js
@@ -0,0 +1,337 @@
+var jsPsychHtmlSliderSVOMulti = (function (jspsych) {
+  'use strict';
+
+  var _package = {
+    name: "@jspsych/plugin-html-slider-svo-multi",
+    version: "2.0.0",
+    description: "a jspsych plugin for free response survey questions",
+    type: "module",
+    main: "dist/index.cjs",
+    exports: {
+      import: "./dist/index.js",
+      require: "./dist/index.cjs"
+    },
+    typings: "dist/index.d.ts",
+    unpkg: "dist/index.browser.min.js",
+    files: [
+      "src",
+      "dist"
+    ],
+    source: "src/index.ts",
+    scripts: {
+      test: "jest",
+      "test:watch": "npm test -- --watch",
+      tsc: "tsc",
+      build: "rollup --config",
+      "build:watch": "npm run build -- --watch"
+    },
+    repository: {
+      type: "git",
+      url: "git+https://github.com/jspsych/jsPsych.git",
+      directory: "packages/plugin-html-slider-svo-multi"
+    },
+    author: "Josh de Leeuw",
+    license: "MIT",
+    bugs: {
+      url: "https://github.com/jspsych/jsPsych/issues"
+    },
+    homepage: "https://www.jspsych.org/latest/plugins/html-slider-response",
+    peerDependencies: {
+      jspsych: ">=7.1.0"
+    },
+    devDependencies: {
+      "@jspsych/config": "^3.0.0",
+      "@jspsych/test-utils": "^1.2.0"
+    }
+  };
+
+  const info = {
+    name: "html-slider-svo-multi",
+    version: _package.version,
+    parameters: {
+      stimulus_upper: {
+        type: jspsych.ParameterType.HTML_STRING,
+        default: '85, 85, 85, 85, 85, 85, 85, 85, 85'
+      },
+      stimulus_lower: {
+        type: jspsych.ParameterType.HTML_STRING,
+        default: '85, 85, 85, 85, 85, 85, 85, 85, 85'
+      },
+      questions: {
+        type: jspsych.ParameterType.COMPLEX,
+        array: true,
+        nested: {
+          stimulus_upper: {
+            type: jspsych.ParameterType.HTML_STRING,
+            default: '85, 85, 85, 85, 85, 85, 85, 85, 85'
+          },
+          stimulus_lower: {
+            type: jspsych.ParameterType.HTML_STRING,
+            array: true,
+            default: '85, 85, 85, 85, 85, 85, 85, 85, 85'
+          }
+        }
+      },
+      label_upper: {
+        type: jspsych.ParameterType.HTML_STRING,
+        default: "You</br>receive",
+        array: false
+      },
+      label_lower: {
+        type: jspsych.ParameterType.HTML_STRING,
+        default: "Other</br>receive",
+        array: false
+      },
+      slider_width: {
+        type: jspsych.ParameterType.INT,
+        default: 500
+      },
+      // box_width: {
+      //   type: jspsych.ParameterType.INT,
+      //   default: 530
+      // },
+      button_label: {
+        type: jspsych.ParameterType.STRING,
+        default: "Continue",
+        array: false
+      },
+      require_movement: {
+        type: jspsych.ParameterType.BOOL,
+        default: false
+      },
+      prompt: {
+        type: jspsych.ParameterType.HTML_STRING,
+        default: null
+      },
+      stimulus_duration: {
+        type: jspsych.ParameterType.INT,
+        default: null
+      },
+      trial_duration: {
+        type: jspsych.ParameterType.INT,
+        default: null
+      },
+      response_ends_trial: {
+        type: jspsych.ParameterType.BOOL,
+        default: true
+      }
+    },
+    data: {
+      rt: {
+        type: jspsych.ParameterType.INT
+      },
+      response: {
+        type: jspsych.ParameterType.INT
+      },
+      stimulus: {
+        type: jspsych.ParameterType.HTML_STRING
+      },
+      slider_start: {
+        type: jspsych.ParameterType.INT
+      }
+    }
+  };
+  class HtmlSliderResponsePlugin {
+    constructor(jsPsych) {
+      this.jsPsych = jsPsych;
+    }
+    static info = info;
+
+    trial(display_element, trial) {
+      var half_thumb_width = 7.5;
+      // var tmp1 = '85, 85, 85, 85, 85, 85, 85, 85, 85';
+      var tmp_ary1 = trial.stimulus_upper.split(',');
+      // var tmp2 = '85, 76, 68, 59, 50, 41, 33, 24, 15';
+      var tmp_ary2 = trial.stimulus_lower.split(',');
+      
+      var html = '<div id="jspsych-html-slider-response-wrapper" style="margin: 100px 0px;';
+      html += 'width:' + trial.slider_width + 'px; ">';
+      if (trial.prompt !== null) {
+        html +=  '<div style="text-align: start";>' + trial.prompt + '</div>';
+      }
+
+      
+      for (var i = 0; i < trial.questions.length; i++) {
+        // var question = trial.questions[i];
+        var tmp_ary1 = trial.questions[i].stimulus_upper;
+        var tmp_ary2 = trial.questions[i].stimulus_lower;
+
+        html += '<div id="jspsych-html-slider-response-stimulus">';
+        html += '<div class="container">'
+        html += '<div class="number-grid"> <div></div> <!-- ラベルのスペースを空ける --> <div class="number">1</div> <div class="number">2</div> <div class="number">3</div> <div class="number">4</div> <div class="number">5</div> <div class="number">6</div> <div class="number">7</div> <div class="number">8</div> <div class="number">9</div> </div>';
+        html += '<div class="grid"><div class="label">'+ trial.label_upper +'</div> <div class="box">'+ tmp_ary1[0] +'</div> <div class="box">'+ tmp_ary1[1] +'</div> <div class="box">'+ tmp_ary1[2] +'</div> <div class="box">'+ tmp_ary1[3] +'</div> <div class="box">'+ tmp_ary1[4] +'</div> <div class="box">'+ tmp_ary1[5] +'</div> <div class="box">'+ tmp_ary1[6] +'</div> <div class="box">'+ tmp_ary1[7] +'</div> <div class="box">'+ tmp_ary1[8] +'</div></div>'
+        html += '<div class="grid"><div></div><div class="slider-container"> <span class="slider-value" id="sliderValue'+ i +'">5</span>';
+        html += '<input type="range" id="jspsych-html-slider-response-response-Q' + i + '" class="slider jspsych-slider value="'+ 5 + '" min="'+ 1 + '" max="'  + 9 +  '" step="' + 1 +'"></input> </div></div>';
+        html += '<div class="grid"><div class="label">'+ trial.label_lower +'</div> <div class="box bottom-box">'+ tmp_ary2[0] +'</div> <div class="box bottom-box">'+ tmp_ary2[1] +'</div> <div class="box bottom-box">'+ tmp_ary2[2] +'</div> <div class="box bottom-box">'+ tmp_ary2[3] +'</div> <div class="box bottom-box">'+ tmp_ary2[4] +'</div> <div class="box bottom-box">'+ tmp_ary2[5] +'</div> <div class="box bottom-box">'+ tmp_ary2[6] +'</div> <div class="box bottom-box">'+ tmp_ary2[7] +'</div> <div class="box bottom-box">'+ tmp_ary2[8] +'</div></div>'
+        html += "</div>"; // container
+        html += "</div>"; //stimulus
+        html += '<hr style="margin-bottom:1.5em; width:'+  trial.slider_width + 'px;"></hr>'
+      }
+
+
+
+      html += "</div>"; //wrapper
+
+      html += '<button id="jspsych-html-slider-response-next" class="jspsych-btn" ' + (trial.require_movement ? "disabled" : "") + ">" + trial.button_label + "</button>";
+      display_element.innerHTML = html;
+      var response = {
+        rt: null,
+        response: null
+      };
+      
+      // require movement check
+      if (trial.require_movement) {
+        var query_count = 0;
+        const check_query_count = () => {
+          query_count += 1;
+          if (query_count == 3){
+            enable_button()
+          }
+        };
+        const enable_button = () => {
+          display_element.querySelector(
+            "#jspsych-html-slider-response-next"
+          ).disabled = false;
+        };
+        
+        const elements1 = document.querySelectorAll('[id^="jspsych-html-slider-response-response-Q"]');
+        const sliderIds = Array.from(elements1).map(elements1 => elements1.id);
+
+        sliderIds.forEach(sliderId => {
+          const slider = display_element.querySelector(`#${sliderId}`);
+      
+          if (slider) { 
+              slider.addEventListener("mousedown", check_query_count);
+              slider.addEventListener("touchstart", check_query_count);
+              slider.addEventListener("change", check_query_count);
+          } else {
+              console.warn(`Slider with ID ${sliderId} not found.`);
+          }
+        });
+
+      }
+
+      const end_trial = () => {
+        var trialdata = {
+          rt: response.rt,
+          stimulus: trial.stimulus,
+          slider_start: trial.slider_start,
+          response: response.response
+        };
+        this.jsPsych.finishTrial(trialdata);
+      };
+      
+      // From here（slider number option）スライダーの数値を監視するコード
+      const elements1 = document.querySelectorAll('[id^="jspsych-html-slider-response-response-Q"]');
+      const sliderIds = Array.from(elements1).map(elements1 => elements1.id);
+      const elements2 = document.querySelectorAll('[id^="sliderValue"]');
+      const sliderValueIds = Array.from(elements2).map(elements2 => elements2.id);
+
+      sliderIds.forEach((sliderId, index) => {
+        const slider = document.getElementById(sliderId);
+        const sliderValue = document.getElementById(sliderValueIds[index]);
+
+        // initial value
+        updateSliderValuePosition(slider, sliderValue, slider.value);
+
+        slider.addEventListener('input', function() {
+            sliderValue.textContent = this.value;
+            updateSliderValuePosition(this, sliderValue, this.value);
+        });
+
+        // windows resize value
+        window.addEventListener('resize', function() {
+            updateSliderValuePosition(slider, sliderValue, slider.value);
+        });
+      });
+
+      // 値表示の位置を更新する関数
+      function updateSliderValuePosition(slider, sliderValue, value) {
+        const sliderWidth = slider.offsetWidth;
+        const min = parseInt(slider.min);
+        const max = parseInt(slider.max);
+        const percentage = (value - min) / (max - min);
+        const valuePosition = percentage * sliderWidth;
+
+        // 調整用
+        sliderValue.style.left = `calc(${percentage * 100}% - 10px)`;
+      }
+      // here
+
+      display_element.querySelector("#jspsych-html-slider-response-next").addEventListener("click", () => {
+        var endTime = performance.now();
+        response.rt = Math.round(endTime - startTime);
+
+        // get response
+        var choice_data = display_element.querySelectorAll(".jspsych-slider");
+        choice_data = Array.from(choice_data).map(choice_data => choice_data.value);
+        response.response = choice_data;
+
+        if (trial.response_ends_trial) {
+          end_trial();
+        } else {
+          display_element.querySelector(
+            "#jspsych-html-slider-response-next"
+          ).disabled = true;
+        }
+      });
+      if (trial.stimulus_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(() => {
+          display_element.querySelector(
+            "#jspsych-html-slider-response-stimulus"
+          ).style.visibility = "hidden";
+        }, trial.stimulus_duration);
+      }
+      if (trial.trial_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
+      }
+      var startTime = performance.now();
+    }
+
+
+
+
+    simulate(trial, simulation_mode, simulation_options, load_callback) {
+      if (simulation_mode == "data-only") {
+        load_callback();
+        this.simulate_data_only(trial, simulation_options);
+      }
+      if (simulation_mode == "visual") {
+        this.simulate_visual(trial, simulation_options, load_callback);
+      }
+    }
+    create_simulation_data(trial, simulation_options) {
+      const default_data = {
+        stimulus: trial.stimulus,
+        slider_start: trial.slider_start,
+        response: this.jsPsych.randomization.randomInt(trial.min, trial.max),
+        rt: this.jsPsych.randomization.sampleExGaussian(500, 50, 1 / 150, true)
+      };
+      const data = this.jsPsych.pluginAPI.mergeSimulationData(default_data, simulation_options);
+      this.jsPsych.pluginAPI.ensureSimulationDataConsistency(trial, data);
+      return data;
+    }
+    simulate_data_only(trial, simulation_options) {
+      const data = this.create_simulation_data(trial, simulation_options);
+      this.jsPsych.finishTrial(data);
+    }
+    simulate_visual(trial, simulation_options, load_callback) {
+      const data = this.create_simulation_data(trial, simulation_options);
+      const display_element = this.jsPsych.getDisplayElement();
+      this.trial(display_element, trial);
+      load_callback();
+      if (data.rt !== null) {
+        const el = display_element.querySelector("input[type='range']");
+        setTimeout(() => {
+          this.jsPsych.pluginAPI.clickTarget(el);
+          el.valueAsNumber = data.response;
+        }, data.rt / 2);
+        this.jsPsych.pluginAPI.clickTarget(display_element.querySelector("button"), data.rt);
+      }
+    }
+  }
+
+  return HtmlSliderResponsePlugin;
+
+})(jsPsychModule);
+
